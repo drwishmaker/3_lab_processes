e283e1b86af520ff18260ad233d681d1ee6d01bf
Artemy Krupsky <4xel1337@gmail.com>
2020-06-13 22:36:52 +0300

    Добавьте файлы проекта.

---
+++ 3_lab_processes/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace _3_lab_processes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFile = new System.Windows.Forms.Button();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxDefault = new System.Windows.Forms.TextBox();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonFile
+            // 
+            this.buttonFile.Location = new System.Drawing.Point(12, 285);
+            this.buttonFile.Name = "buttonFile";
+            this.buttonFile.Size = new System.Drawing.Size(541, 23);
+            this.buttonFile.TabIndex = 0;
+            this.buttonFile.Text = "Choose file";
+            this.buttonFile.UseVisualStyleBackColor = true;
+            this.buttonFile.Click += new System.EventHandler(this.buttonFile_Click);
+            // 
+            // textBoxDefault
+            // 
+            this.textBoxDefault.Location = new System.Drawing.Point(12, 25);
+            this.textBoxDefault.Multiline = true;
+            this.textBoxDefault.Name = "textBoxDefault";
+            this.textBoxDefault.ReadOnly = true;
+            this.textBoxDefault.Size = new System.Drawing.Size(265, 254);
+            this.textBoxDefault.TabIndex = 1;
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(288, 25);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.Size = new System.Drawing.Size(265, 254);
+            this.textBoxResult.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(285, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 314);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.textBoxDefault);
+            this.Controls.Add(this.buttonFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFile;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.TextBox textBoxDefault;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 3_lab_processes/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_lab_processes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool checker = false;
+
+        private void buttonFile_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string textFile;
+                int numberOfLines;
+                string[] textResources = null;
+                int[] resources = null;
+                int[] resourceCount = null;
+                int[] need = null;
+
+                string[] alphabet = new string[] { "A", "B", "C", "D", "E", "F", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+                string filename = ofd.FileName;
+
+                try
+                {
+                    using (var streamReader = new StreamReader(filename, Encoding.UTF8))
+                    {
+                        textFile = streamReader.ReadToEnd();
+                        textBoxDefault.Text = textFile + Environment.NewLine;
+                        string ResoursesLine = File.ReadLines(filename).Skip(0).First();
+                        numberOfLines = File.ReadAllLines(filename).Length - 1;
+
+                        textResources = ResoursesLine.Split(' ');
+                        resources = new int[textResources.Length];
+
+                        for (int i = 0; i != resources.Length; i++)
+                        {
+                            resources[i] = Convert.ToInt32(textResources[i]);
+                        }
+
+                        resourceCount = new int[textResources.Length];
+                        need = new int[textResources.Length];
+                        textBoxResult.Text = "Available resources" + Environment.NewLine;
+
+                        for (int i = 1; i != numberOfLines + 1; i++)
+                        {
+                            string Line = File.ReadLines(filename).Skip(i).First();
+                            string[] Process = Line.Split(' ');
+
+                            for (int k = 1; k != textResources.Length + 1; k++)
+                            {
+                                resourceCount[k - 1] = Convert.ToInt32(Process[k]);
+                                resources[k - 1] -= resourceCount[k - 1];
+                            }
+                        }
+
+                        for (int i = 0; i != resources.Length; i++)
+                        {
+                            textBoxResult.Text += Convert.ToString(resources[i]) + " ";
+                        }
+
+                        bool[] wasChecked = new bool[numberOfLines];
+
+                        for (int a = 0; a != numberOfLines; a++)
+                        {
+                            wasChecked[a] = false;
+                        }
+
+                        for (int i = 1; i != numberOfLines + 1; i++)
+                        {
+                            string line = File.ReadLines(filename).Skip(i).First();
+                            string[] process = line.Split(' ');
+
+                            for (int k = 1; k != textResources.Length + 1; k++)
+                            {
+                                resourceCount[k - 1] = Convert.ToInt32(process[k]);
+                            }
+
+                            for (int k = textResources.Length + 1; k != process.Length; k++)
+                            {
+                                need[k - textResources.Length - 1] = Convert.ToInt32(process[k]);
+                            }
+
+                            int checkLength = 0;
+
+                            for (int a = 0; a != resources.Length; a++)
+                            {
+                                if ((resources[a] + resourceCount[a] >= need[a]) && wasChecked[i - 1] == false)
+                                {
+                                    checkLength++;
+                                }
+
+                                if (checkLength == resources.Length)
+                                {
+                                    textBoxResult.Text += Environment.NewLine + "Started process " + alphabet[i - 1] + ": " + Environment.NewLine;
+
+                                    wasChecked[i - 1] = true;
+
+                                    for (int j = 0; j != resources.Length; j++)
+                                    {
+                                        resources[j] += resourceCount[j];
+                                        textBoxResult.Text += Convert.ToString(resources[j]) + " ";
+                                    }
+
+                                    for (int c = 0; c != numberOfLines; c++)
+                                    {
+                                        if (wasChecked[c] == false)
+                                            checker = true;
+                                    }
+
+                                    if (checker != false)
+                                    {
+                                        i = 0;
+                                    }
+
+                                    else
+                                    {
+                                        return;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Incorrect data from file");
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Unable to read file");
+            }
+
+            if (checker != false)
+            {
+                textBoxResult.Text += Environment.NewLine + "This system is stable ";
+            }
+
+            else
+            {
+                textBoxResult.Text += Environment.NewLine + "Unstable system. Deadlock ";
+            }
+        }
+
+    }
+}
+
